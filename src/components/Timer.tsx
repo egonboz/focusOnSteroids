0d7471b6abb0749295b31a95d0e88ce3f05dc0aa
Egonboz <egonboz@gmail.com>
2025-06-01 13:30:39 +0200

    ✨ ADDED: new pomodoro timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+const Timer: React.FC = () => {
+    const cycle: [number, number, number, number] = [25, 5, 15, 5];
+    const [phaseIndex, setPhaseIndex] = useState<number>(0);
+    const [secondsLeft, setSecondsLeft] = useState<number>(cycle[0] * 60);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+
+    useEffect (() => {
+        let interval: NodeJS.Timeout;
+
+        if ( isRunning && secondsLeft > 0 ) {
+            interval = setInterval(() => {
+                setSecondsLeft(prev => prev - 1);
+            }, 1000);
+        }
+
+        if ( isRunning && secondsLeft === 0 ) {
+                setTimeout(() => {
+                    const nextPhase = (phaseIndex + 1) % cycle.length;
+                    setPhaseIndex(nextPhase);
+                    setSecondsLeft(cycle[nextPhase] * 60);
+                }, 1000);        
+            }
+
+        return () => clearInterval(interval);
+
+    }, [isRunning, secondsLeft, phaseIndex]);
+
+    const minutes = String(Math.floor(secondsLeft / 60)).padStart(2, '0');
+    const seconds = String(secondsLeft % 60).padStart(2, '0');
+
+    const handleStart = () => setIsRunning(true);
+    const handleStop = () => setIsRunning(false);
+
+    const handleSetTime = (minutes: number) => {
+        setIsRunning(false);
+        setSecondsLeft(minutes * 60);
+    };
+
+    return (
+        <div className="flex flex-col items-center gap-6">
+            <div className="flex flex-row gap-2">
+                <div className="flex flex-col items-center gap-4">
+                    <div className="bg-gray-100 rounded-lg px-32 py-3 text-xl font-semibold text-gray-800">
+                        {minutes}
+                    </div>
+                    <p>Minutes</p>
+                </div>
+                <div className="flex flex-col items-center gap-4">
+                    <div className="bg-gray-100 rounded-lg px-32 py-3 text-xl font-semibold text-gray-800">
+                        {seconds}
+                    </div>
+                    <p>Seconds</p>
+                </div>
+            </div>
+            <div className="flex flex-row gap-4">
+                <button className="bg-gray-100 rounded-full px-24 py-2.5 text-l font-semibold text-gray-800" onClick={() => handleSetTime(25)}>25'</button>
+                <button className={`${isRunning ? "bg-gray-300" : "bg-gray-100"} rounded-3xl px-24 py-2.5 text-l font-semibold text-gray-800 transition`} onClick={handleStart}>Start</button>
+                <button className={`${!isRunning ? "bg-gray-300" : "bg-gray-100"} rounded-3xl px-24 py-2.5 text-l font-semibold text-gray-800 transition`} onClick={handleStop}>Stop</button>
+                <button className="bg-gray-100 rounded-full px-24 py-2.5 text-l font-semibold text-gray-800" onClick={() => handleSetTime(5)}>5'</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Timer;
