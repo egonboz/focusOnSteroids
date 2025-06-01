d44d145939478e1464e83b9bc2f544356e071e16
Egonboz <egonboz@gmail.com>
2025-06-01 13:31:03 +0200

    ✨ ADDED: basic youtube player component

---
+++ src/components/YoutubePlayer.tsx
@@ -0,0 +1,23 @@
+
+const YoutubePlayer: React.FC = () => {
+
+    return (
+        <div className="flex flex-col gap-6">
+            <p className="text-xl font-semibold text-gray-800">Youtube Player</p>
+            <div className="bg-gray-100 rounded-xl p-4 flex justify-center items-center">
+                <iframe 
+                    className="rounded-md shadow-md"
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/jfKfPfyJRdk?si=DFyWVkUlcOUE4YHx&amp;controls=0" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" 
+                    allowFullScreen />
+            </div>
+        </div>
+    )
+}
+
+export default YoutubePlayer;
