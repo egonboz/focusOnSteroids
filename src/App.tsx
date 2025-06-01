76b551d34eff523296874be251ac6f2fdc5a6276
Egonboz <egonboz@gmail.com>
2025-06-01 13:29:07 +0200

    🎨 App components structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
+import Timer from './components/Timer'
+import WhiteNoiseButtons from './components/WhiteNoiseButtons'
+import YoutubePlayer from './components/YoutubePlayer'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col items-center justify-center gap-6">
+      <NavBar />
+      <h1 className='text text-2xl font-bold text-gray-800'>Focus Session</h1>
+      <Timer />
+      <YoutubePlayer />
+      <WhiteNoiseButtons />
+    </div>
   )
 }
 
